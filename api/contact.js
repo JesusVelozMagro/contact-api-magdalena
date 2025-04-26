5257be0279e18c80b96407e507ffe641a024a9eb
JesusVelozMagro <117239674+JesusVelozMagro@users.noreply.github.com>
2025-04-26 12:09:36 -0400

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       service: 'gmail',
       auth: {
         user: 'dilemagdaweb@gmail.com',
-        pass: 'drvk qiix urky klkt, 
+        pass: 'drvk qiix urky klkt', 
       },
     });
 
